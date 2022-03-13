996c6c27e06f7baaab79e182f2257a0f119fc782
Micelovecat <flora_bux@mail.ru>
2022-03-14 00:02:06 +0300

    Third commit

--- src/main/java/pro/sky/skyprocartapplicationdemo/SkyproCartApplicationDemoApplication.java
+++ src/main/java/pro/sky/skyprocartapplicationdemo/SkyproCartApplicationDemoApplication.java
@@ -1,17 +1,11 @@
 package pro.sky.skyprocartapplicationdemo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SkyproCartApplicationDemoApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SkyproCartApplicationDemoApplication.class, args);
 	}
-
-
 }
--- src/main/java/pro/sky/skyprocartapplicationdemo/configuration/CartConfiguration.java
+++ src/main/java/pro/sky/skyprocartapplicationdemo/configuration/CartConfiguration.java
@@ -1,20 +1,16 @@
 package pro.sky.skyprocartapplicationdemo.configuration;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.annotation.SessionScope;
 import pro.sky.skyprocartapplicationdemo.cart.Cart;
 
-
 @Configuration
 public class CartConfiguration {
-
 
     @Bean
     @SessionScope
     public Cart createCart(){
         return new Cart();
     }
-
 }

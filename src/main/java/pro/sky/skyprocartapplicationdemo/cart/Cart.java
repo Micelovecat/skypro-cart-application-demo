e595a8ad345963ea642a264b9c8fba54e63135e4
Micelovecat <flora_bux@mail.ru>
2022-03-13 23:59:21 +0300

    Second commit

--- src/main/java/pro/sky/skyprocartapplicationdemo/cart/Cart.java
+++ src/main/java/pro/sky/skyprocartapplicationdemo/cart/Cart.java
@@ -1,23 +1,29 @@
 package pro.sky.skyprocartapplicationdemo.cart;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Cart {
 
+
     private final List<Integer> items;
+
 
     public Cart() {
         this.items = new ArrayList<Integer>();
     }
+
 
     public List<Integer> addItems(List<Integer> idsList){
         items.addAll(idsList);
         return idsList;
     }
 
+
     public List<Integer> getItems(){
         return new ArrayList<Integer>(items);
     }
+
 }
--- src/main/java/pro/sky/skyprocartapplicationdemo/service/impl/CartServiceImpl.java
+++ src/main/java/pro/sky/skyprocartapplicationdemo/service/impl/CartServiceImpl.java
@@ -1,27 +1,35 @@
 package pro.sky.skyprocartapplicationdemo.service.impl;
 
+
 import org.springframework.stereotype.Service;
 import pro.sky.skyprocartapplicationdemo.cart.Cart;
 import pro.sky.skyprocartapplicationdemo.service.CartService;
 
+
 import java.util.List;
+
 
 @Service
 public class CartServiceImpl implements CartService {
 
+
     private final Cart cart;
+
 
     public CartServiceImpl(Cart cart) {
         this.cart = cart;
     }
+
 
     @Override
     public List<Integer> addItems(List<Integer> items) {
         return cart.addItems(items);
     }
 
+
     @Override
     public List<Integer> getItems() {
         return cart.getItems();
     }
+
 }
